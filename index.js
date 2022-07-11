2a3498864458f07749391dbd5458bab8e732352a
Tato-182 <108433663+Tato-182@users.noreply.github.com>
2022-07-11 17:26:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
-  console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`);
+  console.log(`Servidor en puerto: ${server.address().port}`);
 });
 
 server.on("error", error => console.log(`Error en servidor: ${error}`));
